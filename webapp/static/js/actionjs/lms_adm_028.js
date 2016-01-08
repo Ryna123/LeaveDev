d742875d5ade5c48f78fd137a33dadba6af3b63d
songdarathana <songdarathana@gmail.com>
2016-01-08 13:40:33 +0700

    add js

--- webapp/static/js/actionjs/lms_adm_028.js
+++ webapp/static/js/actionjs/lms_adm_028.js
@@ -1,3 +1,49 @@
 /**
  * 
  */
+$(document).ready(function() {
+	loading(true);
+	var a = {empId :2};
+	$.ajax({
+		url : "../action/service/lms_adm_028",
+		dataType : "JSON",
+		type : "POST",
+		data :a,
+		success : function(data) {
+			console.log(data.RESP_DATA);
+				var res = data.RESP_DATA['OVERTIME_REC'];
+				if(res.length<=0) {
+					$("tfoot#otFooter").show();
+				} else {
+					$.each(res,function(i,v){
+						
+						var data = {};
+						data['otID'] = i+1;
+						data['otDuration']  = res[i].oTDuration;
+						data['otDate']  = res[i].oTDate;
+						data['otReason']  = res[i].oTReason;
+						data['otStatus'] = res[i].oTStatus_id;
+						data['otType'] = res[i].oTType;
+						if((data['otType'])=='1') {
+							(data['otType'])='<span class="label label-success">Days</span>';
+						} else if((data['otType'])=='2') {
+							(data['otType'])='<span class="label label-danger">Hours</span>';
+						}
+						
+						if((data['otStatus'])=='1') {
+							(data['otStatus'])='Planned';
+						} else if((data['otStatus'])=='4') {
+							(data['otStatus'])='Requested';
+						}
+						$("#lmsAdm028").tmpl(data).appendTo("tbody#listOT").html();
+					
+					})
+				}
+				loading(false);
+			},
+			error : function(data) {
+				console.log(data);
+			}
+
+		});
+});
--- webapp/static/js/actionjs/lms_adm_029.js
+++ webapp/static/js/actionjs/lms_adm_029.js
@@ -1,3 +1,71 @@
 /**
  * 
  */
+$(document).ready(function() {
+	$.ajax({
+		url : "../action/service/lms_adm_029",
+		dataType : "JSON",
+		type : "POST",
+		//data :a,
+		success : function(data) {
+			//console.log(data.RESP_DATA);
+			var res  = data.RESP_DATA['LEAVESTATUS_REC'];
+			$.each(res,function(i,v){
+				var data = {};
+				data['statusID']  = res[i].statusId;
+				data['statusName']  = res[i].statusName;
+				$("#selectStTmpl").tmpl(data).appendTo("#selectSt");
+			})
+			console.log(res);
+		},
+		error : function(data) {
+			console.log(data);
+		}
+	});
+	
+	$("#addOTForm").validationEngine(gbox.ui.validationEngineOptions);
+	$("#addOT").click(function() {
+		
+		if(!$("#addOTForm").validationEngine('validate')) {
+			return false;
+		}	
+		
+		loading(true);
+		var ot = {};
+		var otType = $("#otType").val();
+		var otDuration = $("#otDuration").val();
+		//var otDate = $("#otDate").val().replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '');
+		var otDate = $("#otDate").val();
+		var otReason = $("#otReason").val();
+		var otStatusId = $("#selectSt").val();
+		
+		ot.oTType = otType;
+		ot.oTDuration = otDuration;
+		ot.oTDate = otDate;
+		ot.oTReason = otReason;
+		ot.oTStatus_id =otStatusId;
+		console.log(ot);
+		
+		$.ajax({
+			url : "../action/service/lms_adm_029I",
+			headers: { 
+		        'Accept': 'application/json',
+		        'Content-Type': 'application/json' 
+		    },
+			dataType : "JSON",
+			type : "POST",
+			data :JSON.stringify(ot),
+			success : function(data) {
+				console.log(data);
+				if(data.CODE =='000') {
+					loading(false);
+					maketoast();
+					location.href= "lms_adm_028"; // link to list all requested
+				}
+			},
+			error : function(data) {
+				console.log(data);
+			}
+		});
+	})
+});
