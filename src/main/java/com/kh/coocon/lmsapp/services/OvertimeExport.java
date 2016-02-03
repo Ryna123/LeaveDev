8463abee581ddd925acd932cf8b52e6f498ff595
songdarathana <songdarathana@gmail.com>
2016-02-03 14:20:27 +0700

    add style

--- src/main/java/com/kh/coocon/lmsapp/services/OvertimeExport.java
+++ src/main/java/com/kh/coocon/lmsapp/services/OvertimeExport.java
@@ -11,6 +11,7 @@ import org.apache.poi.hssf.usermodel.HSSFFont;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellRangeAddress;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.sl.usermodel.TextParagraph.FontAlign;
 import org.apache.poi.ss.usermodel.CellStyle;
@@ -39,41 +40,59 @@ public class OvertimeExport extends AbstractExcelView{
 			// sheet.setDisplayGridlines(false);
 			sheet.setDefaultColumnWidth(15);
 			
+			
 			// create style for header cells
 			CellStyle style = workbook.createCellStyle();
 			Font font = workbook.createFont();
-			font.setFontName("Arial");
 			style.setFillForegroundColor(HSSFColor.BLUE_GREY.index); // set background color to Header
 			style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+			style.setAlignment(CellStyle.ALIGN_CENTER);
+			
+			font.setFontName("Arial");
 			font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
 			font.setColor(HSSFColor.WHITE.index);
-			style.setFont(font);
+			font.setFontHeightInPoints((short)13);
 			
+			style.setFont(font);
 
-			// create header row
-			HSSFRow header = sheet.createRow(3);
+			// create Title row
 			HSSFRow topheader = sheet.createRow(1);
-			HSSFCell cell =topheader.createCell(3);
-			cell.setCellValue("Over Time List");
+			HSSFCell topheaderCell =topheader.createCell(1);
+			CellStyle topheaderStyle= workbook.createCellStyle();
+			Font topheaderFont= workbook.createFont();
+			topheaderCell.setCellValue("Over Time List");
+			sheet.addMergedRegion(new CellRangeAddress(1,1,1,3)); //sheet.addMergedRegion(rowFrom,rowTo,colFrom,colTo); Merge Cell from B2 to C2
+			topheaderStyle.setAlignment(CellStyle.ALIGN_CENTER);
+
+			topheaderFont.setFontHeightInPoints((short)13);
+			topheaderFont.setFontName("Arial");
+			topheaderFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+			
+			topheaderStyle.setFont(topheaderFont);
+			topheaderCell.setCellStyle(topheaderStyle);//set style to top header
+			
+			//create header row
+			HSSFRow header = sheet.createRow(3);
 			
 			header.createCell(0).setCellValue("ID");
 			header.getCell(0).setCellStyle(style);
 
-			header.createCell(1).setCellValue("Status_id");
+			header.createCell(1).setCellValue("Date");
 			header.getCell(1).setCellStyle(style);
-
-			header.createCell(2).setCellValue("Date");
+			
+			header.createCell(2).setCellValue("Duration");
 			header.getCell(2).setCellStyle(style);
 
-			header.createCell(3).setCellValue("Duration");
+			header.createCell(3).setCellValue("Reason");
 			header.getCell(3).setCellStyle(style);
 			
-			header.createCell(4).setCellValue("Ot_type");
+			header.createCell(4).setCellValue("Status");
 			header.getCell(4).setCellStyle(style);
 
-			header.createCell(5).setCellValue("Cause");
-			header.getCell(5).setCellStyle(style);
-
+			// create style for each row
+			CellStyle rowStyle= workbook.createCellStyle();
+			rowStyle.setAlignment(CellStyle.ALIGN_CENTER);
+			
 			// create data rows
 			int rowCount = 4;
 			int id=1;
@@ -81,23 +100,32 @@ public class OvertimeExport extends AbstractExcelView{
 			String ottype="";
 			for (OverTime aot : listOT) {
 				HSSFRow aRow = sheet.createRow(rowCount++);
+
+				// set data to each row
 				aRow.createCell(0).setCellValue(id++);
-				if((aot.getoTStatus_id())==1) {
-					otStatus="Planned";
-				} else if((aot.getoTStatus_id())==4) {
-					otStatus="Requested";
-				}
-				aRow.createCell(1).setCellValue(otStatus);
-				aRow.createCell(2).setCellValue(aot.getoTDate());
-				aRow.createCell(3).setCellValue(aot.getoTDuration());
+				aRow.getCell(0).setCellStyle(rowStyle);
+				
+				aRow.createCell(1).setCellValue(aot.getoTDate());
+				aRow.getCell(1).setCellStyle(rowStyle);
+				
 				if((aot.getoTType())==1) {
 					ottype="Day(s)";
 				} else if((aot.getoTType())==2) {
 					ottype="Hour(s)";
 				}
-				aRow.createCell(4).setCellValue(ottype);
-				aRow.createCell(5).setCellValue(aot.getoTReason());
+				aRow.createCell(2).setCellValue(aot.getoTDuration()+"  "+ottype);
+				aRow.getCell(2).setCellStyle(rowStyle);
 				
+				aRow.createCell(3).setCellValue(aot.getoTReason());
+				aRow.getCell(3).setCellStyle(rowStyle);
+				
+				if((aot.getoTStatus_id())==1) {
+					otStatus="Planned";
+				} else if((aot.getoTStatus_id())==4) {
+					otStatus="Requested";
+				}
+				aRow.createCell(4).setCellValue(otStatus);
+				aRow.getCell(4).setCellStyle(rowStyle);
 			}
 			/*
 			 * HSSFRow row = sheet.createRow(1); HSSFCell cell =
