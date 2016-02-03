2655f610b52dee8fc20ddb0bd436be07d08488de
Sabaychitra84 <sabaychitra84@gmail.com>
2016-02-03 08:59:56 +0700

    Udated this

--- src/main/java/com/kh/coocon/lmsapp/dao/UserProfileDaoImpl.java
+++ src/main/java/com/kh/coocon/lmsapp/dao/UserProfileDaoImpl.java
@@ -17,14 +17,14 @@ public class UserProfileDaoImpl extends AbstractDao<Integer, UserProfile> implem
 	@SuppressWarnings("unchecked")
 	public List<UserProfile> findAll() {
 		Criteria crit = createEntityCriteria();
-		crit.addOrder(Order.asc("type"));
+		crit.addOrder(Order.asc("name"));
 		
 		return (List<UserProfile>)crit.list();		
 	}
 
 	public UserProfile findByType(String type) {
 		Criteria crit = createEntityCriteria();
-		crit.add(Restrictions.eq("type", type));
+		crit.add(Restrictions.eq("name", type));
 		
 		return (UserProfile) crit.uniqueResult();
 	}
