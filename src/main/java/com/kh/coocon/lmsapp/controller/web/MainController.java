ae2b2705ecf29fcda0593ac73c954ad46e5d8a44
borith taing <tborith@gmail.com>
2016-02-04 14:21:26 +0700

    d

--- src/main/java/com/kh/coocon/lmsapp/controller/web/MainController.java
+++ src/main/java/com/kh/coocon/lmsapp/controller/web/MainController.java
@@ -91,7 +91,7 @@ public class MainController {
 		return "lms_adm_011";
 	}
 	
-	@RequestMapping(value="/admin/lms_adm_r014", method=RequestMethod.GET)
+	@RequestMapping(value="/admin/lms_adm_014", method=RequestMethod.GET)
 	public String showLms_adm_014(ModelMap m){
 		m.addAttribute("user", ssoidUtil.getPrincipal());		
 		return "lms_adm_014";
