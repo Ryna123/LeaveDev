9239ef3cbf8ee9e3886bda0db4479e8aa7c8dac2
borith taing <tborith@gmail.com>
2016-03-09 16:13:13 +0700

    update some code in OrgUer

--- webapp/static/js/actionjs/lms_adm_015.js
+++ webapp/static/js/actionjs/lms_adm_015.js
@@ -4,7 +4,7 @@
 var lms_adm_015 ={};
 var _orgid = '';
 var _managerId = '';
-
+var _table=null;
 $(document).ready(function() {
 	lms_adm_015.loadData ();
 	
@@ -67,6 +67,7 @@ lms_adm_015.loadData = function(){
 }
 lms_adm_015.LoadUserInDepartment = function(input){
 	loading(true);
+	console.log(input);
 	//var a ={orgId:"2",managerId:"1"};
 	$.ajax({
 		url : "../action/service/lms_adm_016",
@@ -74,7 +75,41 @@ lms_adm_015.LoadUserInDepartment = function(input){
 		type : "POST",
 		data :input,
 		success : function(dat) {
-			$("tbody#userArea").empty();
+			if(_table==null){
+				var values=dat.RESP_DATA['USR_REC'];
+				console.log(values);
+				
+				_table = $("#tblOrg").DataTable({
+					"pagingType": "full_numbers",
+					data:dat.RESP_DATA['USR_REC'],
+					"dom": '<"top"i>rt<"bottom"lp>',
+					columns:[
+					         {"data":"identifier", 
+					        	 //disable sort 
+					        	 "orderable": false, 
+					        	 //set ClassName to this td
+					        	 className:"dataRow", 
+					        	 //Disable search on this column
+					        	 "bSearchable": false,
+					        	 "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+					        			 $(nTd).html("<input type='checkbox' id='chbBox' value="+sData+">");
+					             }
+					        },
+					         {"data":"identifier"},
+					         {"data":"firstName"},
+					         {"data":"lastName"},
+					         {"data":"phone"},
+					         {"data":"email"}
+					]
+				});
+			}else{
+				_table.clear().draw();
+				_table.rows.add(dat.RESP_DATA['USR_REC']);
+				_table.columns.adjust().draw();
+			}
+			
+			
+			/*$("tbody#userArea").empty();
 			console.log(dat.RESP_DATA);
 			var res = dat.RESP_DATA['USR_REC'];
 			$.each(res,function(i,v){
@@ -87,7 +122,7 @@ lms_adm_015.LoadUserInDepartment = function(input){
 				data['PHONE'] = res[i].phone;
 				data['EMAIL'] = res[i].email;
 				$("#userTamplate").tmpl(data).appendTo("tbody#userArea").html();
-			});
+			});*/
 		}
 	
 	});
