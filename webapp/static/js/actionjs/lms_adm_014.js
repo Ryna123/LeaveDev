57d2848be17d333971cb51831eee8f60935709f9
borith taing <tborith@gmail.com>
2016-03-10 10:56:43 +0700

    done sth

--- webapp/static/js/actionjs/lms_adm_014.js
+++ webapp/static/js/actionjs/lms_adm_014.js
@@ -16,7 +16,13 @@ $(document).ready(function(){
 		lms_adm_009p.loadData();
 	});
 	$('#btnOk').click(function(){
-		lms_adm_009p.selectDataTree($('#txtOrgId').val());
+		var orgid = $('#txtOrgId').val();
+		if(orgid==0 || orgid==null){
+			_employeeManagement.loadEmpData();
+		}else{
+			lms_adm_009p.selectDataTree(orgid);
+		}
+		
 	});
 });
 
@@ -26,46 +32,53 @@ _employeeManagement.loadEmpData=function(){
 		dataType: "JSON",
 		type:"GET",
 		success:function(data){
-			//alert(JSON.stringify(data.List));
-			var listData=data.List;
-			//var count = Object.keys(listData).length;
-			console.log(data.List);
-			var values={"data":listData};
-			//Covert json to String 
-			//var dataSet=JSON.stringify(values);
-			//covert String to json
-			//var dd=JSON.parse(dataSet);
-			//console.log(values);
-			//$('#hrTable tbody tr').remove();
-			_table = $('#hrTable').DataTable({
-				"pagingType": "full_numbers",
-				data:values["data"],
-				 "dom": '<"top"i>rt<"bottom"lp>',
-				 "scrollY":"400px",
-				"scrollCollapse":true,
-				columns:[
-				        {"data":"id","bSearchable": false},
-				        {"data":"status",className:"btnStatus","bSearchable": false,
-				        	 "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
-				        		 if(oData.status.toLowerCase()=='active'){
-				        			 $(nTd).html("<span class='glyphicon glyphicon-ok' data-toggle='tooltip' " +
-				                 		"data-placement='top' title='' data-original-title='Active' value='1'></span>");
-				        		 }else{
-				        			 $(nTd).html("<span class='glyphicon glyphicon-remove' data-toggle='tooltip' " +
-				                 		"data-placement='top' title='' data-original-title='Active' value='0'></span>");
-				        		 }
-				             }
-				        },
-				        {"data":"firstName"},
-				        {"data":"lastName"},
-				        {"data":"phone","bSearchable": false},
-				        {"data":"email","bSearchable": false},
-				        {"data":"department"},
-				        {"data":"contract","bSearchable": false},
-				        {"data":"manager","bSearchable": false}
-				],
-				//"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
-			});
+			if(_table!=null){
+				_table.clear().draw();
+				_table.rows.add(data.List);
+				_table.columns.adjust().draw();
+			}else{
+				//alert(JSON.stringify(data.List));
+				var listData=data.List;
+				//var count = Object.keys(listData).length;
+				console.log(data.List);
+				var values={"data":listData};
+				//Covert json to String 
+				//var dataSet=JSON.stringify(values);
+				//covert String to json
+				//var dd=JSON.parse(dataSet);
+				//console.log(values);
+				//$('#hrTable tbody tr').remove();
+				_table = $('#hrTable').DataTable({
+					"pagingType": "full_numbers",
+					data:values["data"],
+					 "dom": '<"top"i>rt<"bottom"lp>',
+					 "scrollY":"400px",
+					"scrollCollapse":true,
+					columns:[
+					        {"data":"id","bSearchable": false},
+					        {"data":"status",className:"btnStatus","bSearchable": false,
+					        	 "fnCreatedCell": function (nTd, sData, oData, iRow, iCol) {
+					        		 if(oData.status.toLowerCase()=='active'){
+					        			 $(nTd).html("<span class='glyphicon glyphicon-ok' data-toggle='tooltip' " +
+					                 		"data-placement='top' title='' data-original-title='Active' value='1'></span>");
+					        		 }else{
+					        			 $(nTd).html("<span class='glyphicon glyphicon-remove' data-toggle='tooltip' " +
+					                 		"data-placement='top' title='' data-original-title='Active' value='0'></span>");
+					        		 }
+					             }
+					        },
+					        {"data":"firstName"},
+					        {"data":"lastName"},
+					        {"data":"phone","bSearchable": false},
+					        {"data":"email","bSearchable": false},
+					        {"data":"department"},
+					        {"data":"contract","bSearchable": false},
+					        {"data":"manager","bSearchable": false}
+					],
+					//"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
+				});
+			}
+			
 		}
 	});
 }
